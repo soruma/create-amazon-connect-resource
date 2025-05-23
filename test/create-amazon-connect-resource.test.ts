5a496d0de025baebc0398c67984005351efa6faa
Shingo Hosotani <hosotani.shingo@uhuru.jp>
2025-05-23 16:55:37 +0900

    Add test for isCreateHierarchy

--- test/create-amazon-connect-resource.test.ts
+++ test/create-amazon-connect-resource.test.ts
@@ -3,19 +3,39 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { CreateAmazonConnectResourceStack } from '../lib/create-amazon-connect-resource-stack';
 
 describe('CreateAmazonConnectResourceStack', () => {
-  test("matches the snapshot", () => {
-    const app = new cdk.App();
+  describe('Matches the snapshot', () => {
+    test('Create data storage and hierarchy', () => {
+      const app = new cdk.App();
 
-    const stack = new CreateAmazonConnectResourceStack(app, 'CreateAmazonConnectResourceStack-test', {
-      env: {
-        account: '000000000000',
-        region: 'us-east-1',
-      },
-      connectInstanceAlias: 'test',
-      isCreateHierarchy: true,
+      const stack = new CreateAmazonConnectResourceStack(app, 'CreateAmazonConnectResourceStack-test', {
+        env: {
+          account: '000000000000',
+          region: 'us-east-1',
+        },
+        connectInstanceAlias: 'test',
+        isCreateDataStorageBucket: true,
+        isCreateHierarchy: true,
+      });
+
+      const template = Template.fromStack(stack);
+      expect(template.toJSON()).toMatchSnapshot();
     });
 
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+    test('Create data storage, not create hierarchy', () => {
+      const app = new cdk.App();
+
+      const stack = new CreateAmazonConnectResourceStack(app, 'CreateAmazonConnectResourceStack-test', {
+        env: {
+          account: '000000000000',
+          region: 'us-east-1',
+        },
+        connectInstanceAlias: 'test',
+        isCreateDataStorageBucket: true,
+        isCreateHierarchy: false,
+      });
+
+      const template = Template.fromStack(stack);
+      expect(template.toJSON()).toMatchSnapshot();
+    });
   });
 });

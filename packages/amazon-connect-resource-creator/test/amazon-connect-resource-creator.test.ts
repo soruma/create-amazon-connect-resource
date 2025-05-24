7e708da9c23ab507f08f98359328475326b70d89
Shingo Hosotani <hosotani.shingo@uhuru.jp>
2025-05-24 22:26:24 +0900

    Add amazon-connect-resource-creator

---
+++ packages/amazon-connect-resource-creator/test/amazon-connect-resource-creator.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmazonConnectResourceCreator from '../lib/amazon-connect-resource-creator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amazon-connect-resource-creator-stack.ts
+test('SQS Queue Created', () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new AmazonConnectResourceCreator.AmazonConnectResourceCreatorStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
